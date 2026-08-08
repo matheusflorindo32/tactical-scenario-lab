bb1d7155c8f7a8497dddc9a6c7ad79c5cdf5d8d1
MATHEUS FLORINDO DE DEUS <matheusdideusf@gmail.com>
2026-08-08 13:56:04 -0300

    style: align calculator test with Pint

--- tests/Unit/AssessmentScoreCalculatorTest.php
+++ tests/Unit/AssessmentScoreCalculatorTest.php
@@ -9,7 +9,7 @@ class AssessmentScoreCalculatorTest extends TestCase
 {
     public function test_weighted_base_score_is_deterministic(): void
     {
-        $calculator = new AssessmentScoreCalculator();
+        $calculator = new AssessmentScoreCalculator;
 
         $this->assertSame(86.00, $calculator->calculateBase([
             ['score' => 80.00, 'weight' => 40.00],
@@ -19,7 +19,7 @@ class AssessmentScoreCalculatorTest extends TestCase
 
     public function test_final_score_applies_penalties_adjustment_and_clamp(): void
     {
-        $calculator = new AssessmentScoreCalculator();
+        $calculator = new AssessmentScoreCalculator;
 
         $this->assertSame(81.00, $calculator->calculateFinal(86.00, 7.00, 2));
         $this->assertSame(0.00, $calculator->calculateFinal(5.00, 20.00, -10));
@@ -28,7 +28,7 @@ class AssessmentScoreCalculatorTest extends TestCase
 
     public function test_result_respects_threshold_and_automatic_fail_precedence(): void
     {
-        $calculator = new AssessmentScoreCalculator();
+        $calculator = new AssessmentScoreCalculator;
 
         $this->assertSame('failed', $calculator->result(95.00, 70.00, true));
         $this->assertSame('passed', $calculator->result(70.00, 70.00, false));
