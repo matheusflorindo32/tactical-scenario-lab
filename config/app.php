985da3e1aa484536ad5a5de149d14ff8bf718635
MATHEUS FLORINDO DE DEUS <matheusdideusf@gmail.com>
2026-08-09 14:43:35 -0300

    chore(m9): remove incidental config comment drift

--- config/app.php
+++ config/app.php
@@ -34,8 +34,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within every
-    | request. If disabled, a simple generic error page is shown.
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
     |
     */
 
