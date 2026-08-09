1e52381304d7d5e44bd91f0b2343022e8125297c
MATHEUS FLORINDO DE DEUS <matheusdideusf@gmail.com>
2026-08-09 11:50:06 -0300

    feat: add M8 knowledge catalog shell

---
+++ config/knowledge.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'categories' => [
+        'getting-started' => 'Primeiros passos',
+        'operation' => 'Operação',
+        'assessment' => 'Avaliação & Debrief',
+        'governance' => 'Histórico & Governança',
+    ],
+
+    'audiences' => [
+        'instructor',
+        'evaluator',
+        'manager',
+        'administrator',
+    ],
+
+    'articles' => [],
+];
