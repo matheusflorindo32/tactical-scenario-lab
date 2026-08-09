bbb49613b75b16b1866e5c145dfe46a182eed8e9
MATHEUS FLORINDO DE DEUS <matheusdideusf@gmail.com>
2026-08-09 12:05:52 -0300

    feat: render institutional knowledge article view

--- app/Http/Controllers/KnowledgeController.php
+++ app/Http/Controllers/KnowledgeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Knowledge\KnowledgeRepository;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use Symfony\Component\HttpFoundation\Response;
 
 final class KnowledgeController extends Controller
 {
@@ -30,12 +29,21 @@ final class KnowledgeController extends Controller
         ]);
     }
 
-    public function show(string $slug, KnowledgeRepository $repository): Response
+    public function show(string $slug, KnowledgeRepository $repository): View
     {
         $article = $repository->find($slug);
 
         abort_if($article === null, 404);
 
-        return response($article->html);
+        $relatedArticles = collect($article->related)
+            ->map(fn (string $relatedSlug) => $repository->find($relatedSlug))
+            ->filter()
+            ->values();
+
+        return view('knowledge.show', [
+            'article' => $article,
+            'relatedArticles' => $relatedArticles,
+            'categories' => (array) config('knowledge.categories', []),
+        ]);
     }
 }
